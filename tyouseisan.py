3ebf2884e521305875ed1da7103ee3e025338d13
Cha0704 <amanohasidate.the.ark@gmail.com>
2024-08-28 20:13:05 +0900

    10月の調整さんを取得できるようにした

--- tyouseisan.py
+++ tyouseisan.py
@@ -6,26 +6,30 @@ def main():
     options.add_argument('--headless')
 
     driver = webdriver.Chrome(options=options)
-
-    driver.get('https://chouseisan.com/s?h=5e04f4fe5a8644b193fa06dc00116256')
-
-    table = driver.find_element(By.ID, 'nittei')
-    trs = table.find_elements(By.TAG_NAME,'tr')
-
-    member = []
-    for td in trs[0].find_elements(By.TAG_NAME, "td")[4:]:
-        member.append(td.text)
-
+    urls = [
+        "https://chouseisan.com/s?h=5e04f4fe5a8644b193fa06dc00116256",  # 9月の調整さん
+        "https://chouseisan.com/s?h=94277113268548c693a06e6318a2986c",  # 10月
+    ]
     data = {}
-    for tr in trs[1:-1]:
-        tds = tr.find_elements(By.TAG_NAME, "td")
-        date = tds[0].text.split('(')[0]
-        ok = int(tds[1].text[0])
-        mention = []
-        for i, td in enumerate(tds[4:]):
-            if td.find_element(By.TAG_NAME, "img").get_attribute("src")[-5] != "x":
-                mention.append(member[i])
-        data[date] = { "ok": ok, "mention": mention }
+    for url in urls:
+        driver.get(url)
+        table = driver.find_element(By.ID, 'nittei')
+        trs = table.find_elements(By.TAG_NAME,'tr')
+
+        member = []
+        for td in trs[0].find_elements(By.TAG_NAME, "td")[4:]:
+            member.append(td.text)
+
+
+        for tr in trs[1:-1]:
+            tds = tr.find_elements(By.TAG_NAME, "td")
+            date = tds[0].text.split('(')[0]
+            ok = int(tds[1].text[0])
+            mention = []
+            for i, td in enumerate(tds[4:]):
+                if td.find_element(By.TAG_NAME, "img").get_attribute("src")[-5] != "x":
+                    mention.append(member[i])
+            data[date] = { "ok": ok, "mention": mention }
 
     driver.quit()
     return data
